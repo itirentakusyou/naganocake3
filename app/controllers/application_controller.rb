5af37afd4fa0a2c21d3ec0b6a0b94ecba9241904
EC2 Default User <ec2-user@ip-172-31-21-177.ap-northeast-1.compute.internal>
2023-03-21 02:02:51 +0000

    error解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
-class Customer::ItemsController < ApplicationController
-  def index
-    @total_items = Item.all
-    @items = Item.all.page(params[:page]).per(8)
-  end
-
-  def show
-    @item = Item.find(params[:id])
-    @cart_item = CartItem.new
-  end
-
-  private
-  def items_params
-    parmas.require(:item).permit(:genle_id, :name, :introduction, :price, :is_active, :image_id)
-  end
+class ApplicationController < ActionController::Base
+  
 end
