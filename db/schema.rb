65c82623bf93c2b899da2d5a9f0eabb797c44807
EC2 Default User <ec2-user@ip-172-31-7-212.ap-northeast-1.compute.internal>
2023-03-23 07:59:35 +0000

    S

--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 2023_03_21_071153) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
-  create_table "genres", force: :cascade do |t|
+  create_table "genres", force: :cascade do |t| 
     t.integer "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
