f7c1d216b20bbed203d416686d925f21390894b2
EC2 Default User <ec2-user@ip-172-31-37-102.ap-northeast-1.compute.internal>
2023-03-26 04:39:47 +0000

    update

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,49 +1,49 @@
-class Customers::ShippingAddressesController < ApplicationController
+class Public::AddressesController < ApplicationController
 
     before_action :authenticate_customer!
 
     # 配送先一覧/配送先登録画面
     def index
-        @shipping_address = ShippingAddress.new
-        @shipping_addresses = current_customer.shipping_addresses
+        @shipping_address = Address.new
+        @shipping_addresses = current_customer.addresses
     end
 
     # 配送先登録ボタン
     def create
-        @shipping_address = ShippingAddress.new(shipping_address_params)
+        @shipping_address = Address.new(address_params)
         @shipping_address.customer_id = current_customer.id
         if @shipping_address.save
-            redirect_to customers_shipping_addresses_path
+            redirect_to addresses_path
             flash[:success] = "登録しました。"
         else
-            @shipping_address = ShippingAddress.new
-            @shipping_addresses = current_customer.shipping_addresses
+            @shipping_address = Address.new
+            @shipping_addresses = current_customer.addresses
             render 'index'
         end
     end
 
     # 配送先を削除する
     def destroy
-        shipping_address = ShippingAddress.find(params[:id])
+        shipping_address = Address.find(params[:id])
         shipping_address.destroy
-        redirect_to customers_shipping_addresses_path
+        redirect_to addresses_path
     end
 
     # 配送先編集ボタン
     def edit
-        @shipping_address = ShippingAddress.find(params[:id])
+        @shipping_address = Address.find(params[:id])
     end
 
     # 配送先編集保存ボタン
     def update
-        shipping_address = ShippingAddress.find(params[:id])
-        shipping_address.update(shipping_address_params)
-        redirect_to customers_shipping_addresses_path
+        address = Address.find(params[:id])
+        address.update(address_params)
+        redirect_to addresses_path
     end
 
     private
-    def shipping_address_params
-        params.require(:shipping_address).permit(:customer_id, :name, :postal_code, :address)
+    def address_params
+        params.require(:address).permit(:customer_id, :name, :postal_code, :address)
     end
 
 end
