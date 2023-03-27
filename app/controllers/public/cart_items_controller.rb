8ebf4fa723de8e25781b043fe5172658a455c924
EC2 Default User <ec2-user@ip-172-31-7-212.ap-northeast-1.compute.internal>
2023-03-27 06:51:16 +0000

    3

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,27 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
- 
+
   def index
     @cart_items = current_customer.cart_items
   end
 
   def create
 
-    #if @cart_item
-      #new_amount = @cart_item.amount + cart_item_params[:amount]
+   def create
+  @cart_item = current_customer.cart_items.build(cart_item_params)
+  @cart_items = current_customer.cart_items.all
+  @cart_items.each do |cart_item|
+    if cart_item.item_id == @cart_item.item_id
+      new_quantity = cart_item.quantity + @cart_item.quantity
+      cart_item.update_attribute(:quantity, new_quantity)
+      @cart_item.delete
+    end
+    end
+    @cart_item.save
+   redirect_to :cart_items
+   end
+      #if @cart_item
+      #new_quantity = @cart_item.quantity + cart_item_params[:quantity]
       #@cart_item.update(amount: new_amount)
       #redirect_to cart_items_path
     #else
@@ -41,7 +54,7 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :customer_id)
+    params.require(:cart_item).permit(:item_id, :quantity)
   end
 
 
